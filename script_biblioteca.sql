52af13d21709f796ba748d64e3ca01c7ec0eab6e
Gabrielle Furtado <gabriellerodriguesfurtado@gmail.com>
2026-08-28 20:48:36 -0300

    Finalização do exercício

--- script_biblioteca.sql
+++ script_biblioteca.sql
@@ -98,6 +98,7 @@ VALUES ('Sala de Estudos 3', '12', '2ª andar', 'Disponível');
 
 -- Cadastre na tabela usuario um novo usuário com os seguintes dados: Nome: Fernanda Lima E-mail: fernanda.lima@email.com Senha: 123456 Telefone: 31999990006 Tipo de usuário: utilize um id_tipo_usuario existente Armário: nenhum
 SELECT * FROM usuario;
+ 
 INSERT INTO usuario
 (nome, email, senha, telefone, id_tipo_usuario, id_armario, status)
 VALUES ('Fernanda Lima', 'fernanda.lima@email.com', '123456', '31999990006', 1, NULL, '1');
@@ -106,3 +107,103 @@ VALUES ('Fernanda Lima', 'fernanda.lima@email.com', '123456', '31999990006', 1, 
 SELECT * FROM livro;
 INSERT INTO livro (isbn, titulo, ano_publicacao, id_categoria, id_editora)
 VALUES ('9788550800653', 'Engenharia de Software', 2019, 1, 1);
+
+
+-- 2 PARTE ---------------------------------------------------------------------------
+-- 9. Consultar todos os usuários - Faça uma consulta que apresente todos os dados de todos os usuários cadastrados.
+SELECT * FROM usuario;
+
+-- 10. Consultar informações específicas  - Faça uma consulta que apresente somente as seguintes informações dos usuários:
+SELECT nome, email, telefone FROM usuario;
+
+-- 11. Localizar usuário pelo ID - Faça uma consulta que apresente somente o usuário cujo id_usuario seja igual a 3.
+SELECT * FROM usuario
+WHERE id_usuario = 3;
+
+-- 12. Localizar livro pelo ISBN - Faça uma consulta que apresente o livro cujo ISBN seja: 9788535910663
+SELECT * FROM livro
+WHERE isbn = '9788535910663';
+
+-- 13. Consultar livros por categoria - Faça uma consulta que apresente todos os livros cujo id_categoria seja igual a 2.
+SELECT * FROM livro
+WHERE id_categoria = 2;
+
+-- 14. Consultar empréstimos - Faça uma consulta que apresente somente os empréstimos cujo status seja: Emprestado
+SELECT * FROM emprestimo
+WHERE status = 'Emprestado';
+
+-- 15. Consultar reservas de livros - Faça uma consulta que apresente somente as reservas de livros cujo status seja: Ativa
+SELECT * FROM reserva_livro
+WHERE status = 'Ativa';
+
+-- 16. Consultar espaços disponíveis - Faça uma consulta que apresente somente os espaços cujo status seja: Disponível
+SELECT * FROM espaco
+WHERE status = 'Disponível';
+
+-- 3 PARTE ----------------------------------------------------------------------------------------------------------------------
+-- 17. Alterar telefone - Altere o telefone do usuário cujo id_usuario seja igual a 1 para: ● 31988887777
+SELECT * FROM usuario;
+UPDATE usuario SET telefone = '31988887777'
+WHERE id_usuario = 1; 
+
+-- 18. Alterar mais de uma informação - Altere os seguintes dados do usuário cujo id_usuario seja igual a 2: ● E-mail: maria.souza@biblioteca.com ● Telefone: 31988886666
+SELECT * FROM usuario;
+UPDATE usuario SET email = 'maria.souza@biblioteca.com', telefone = '31988886666'
+WHERE id_usuario = 2; 
+
+-- 19. Alterar status de um armário - Altere o status do armário cujo id_armario seja igual a 4 para: ● Ocupado
+SELECT * FROM armario;
+UPDATE armario SET status = 'Ocupado'
+WHERE id_armario = 4; 
+
+-- 20. Alterar uma categoria - Altere o nome da categoria: ● Banco de Dados para: Banco de Dados e SQL
+SELECT * FROM categoria;
+UPDATE categoria SET nome = 'Banco de Dados e SQL'
+WHERE id_categoria = 4; 
+
+-- 21. Alterar dados de um espaço - Altere o espaço cujo id_espaco seja igual a 3. Novos dados: ● Capacidade: 25 ● Status: Ocupado 
+SELECT * FROM espaco;
+UPDATE espaco SET capacidade = '25', status = 'Ocupado'
+WHERE id_espaco = 3; 
+
+-- 22. Registrar devolução - O empréstimo cujo id_emprestimo seja igual a 4 foi devolvido. Atualize: ● Data de devolução: 2026-08-27 ● Status: Devolvido
+SELECT * FROM emprestimo;
+UPDATE emprestimo SET data_devolucao = '2026-08-27', status = 'Devolvido'
+WHERE id_emprestimo = 4; 
+
+-- 23. Cancelar reserva de livro - Altere o status da reserva de livro cujo id_reserva_livro seja igual a 1 para: ● Cancelada
+SELECT * FROM reserva_livro;
+UPDATE reserva_livro SET status = 'Cancelada'
+WHERE id_reserva_livro = 1; 
+
+-- 24. Confirmar reserva de espaço - Altere o status da reserva de espaço cujo id_reserva_espaco seja igual a 5 de: ● Pendente para: Confirmada
+SELECT * FROM reserva_espaco;
+UPDATE reserva_espaco SET status = 'Confirmada'
+WHERE id_reserva_espaco = 5; 
+
+-- 4 PARTE ---------------------------------------------------------------------------------------------------------------------------------------------
+-- 25. Excluir uma categoria - Primeiro, cadastre na tabela categoria uma categoria chamada:  ● Categoria Teste - Em seguida, exclua ela pelo id gerado.
+SELECT * FROM categoria;
+INSERT INTO categoria (nome)
+VALUES ('Categoria Teste');
+DELETE FROM categoria
+WHERE id_categoria = 7;
+
+-- 26. Excluir um autor - Primeiro, cadastre na tabela autor: ● Autor Teste - Em seguida, exclua ela pelo id gerado.
+SELECT * FROM autor;
+INSERT INTO autor (nome)
+VALUES ('Autor Teste');
+DELETE FROM autor
+WHERE id_autor = 7;
+
+-- 27. Excluir um espaço - Primeiro, cadastre um espaço com os seguintes dados: ● Nome: Sala Temporária ● Capacidade: 5 ● Localização: Térreo ● Status: Disponível - Em seguida, exclua ela pelo id gerado.
+SELECT * FROM espaco;
+INSERT INTO espaco (nome, capacidade, localizacao, status)
+VALUES ('Sala Temporária', '5', 'Térreo', 'Disponível');
+DELETE FROM espaco
+WHERE id_espaco = 8;
+
+-- 27. Excluir um tipo_usuario - Exclua o tipo_usuario de id_tipo_usuario igual a 1. O que aconteceu? Qual a mensagem recebida? O que significa? Como resolver?
+SELECT * FROM tipo_usuario;
+DELETE FROM tipo_usuario
+WHERE id_tipo_usuario = 1;
